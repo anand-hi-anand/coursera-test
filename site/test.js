f10af777f23fdda3a87a2b35eb75f8d074c26bf3
Anand <shashwat.anand@capgemini.com>
2023-02-06 15:56:44 +0530

    Yaha koi DeviPrasad nahi hai

---
+++ site/test.js
@@ -0,0 +1,8 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+
+
+    console.log(names[i].charAt(0).toLowerCase());
+
+}
